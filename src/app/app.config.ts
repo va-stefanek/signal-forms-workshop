3523b1615f1f5e43e406030625878436ec6197ec
Mateusz Stefańczyk <mateusz.stefanczyk@valueadd.pl>
2026-04-20 09:29:38 +0200

    small config improvements

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,8 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import {
+  provideHttpClient, withInterceptorsFromDi
+} from '@angular/common/http';
 
 import { routes } from './app.routes';
 
@@ -9,6 +11,8 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(
+      withInterceptorsFromDi()
+    ),
   ]
 };
